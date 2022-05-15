abbe610df0d7a27a8e468385bce49790ca11f5e5
Thiago Lyon <thiago.lyon.passos@gmail.com>
2022-05-14 21:36:06 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/lyonmovier/LyonmovierApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.lyonmovier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyonmovierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyonmovierApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/lyonmovier/LyonmovierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.lyonmovier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyonmovierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
